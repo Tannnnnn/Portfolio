ef4fb6599cc0e99f118ff8d5b4c5b7f10d046116
Tannnnnn <tan_sweetmulet@hotmail.com>
2018-10-17 19:23:01 +0700

    initial commit

---
+++ src/components/Parallax.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Parallax extends Component {
+  render() {
+    return (
+        <div className='parallax'>
+            <div id='stars1'></div>
+            <div id='stars2'></div>
+            <div id='stars3'></div>
+        </div>
+    )
+  }
+}
+
+export default Parallax
---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Profile extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-Header">
+            Hi , I'm Tan <br /><small>\(^o^)/</small>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Profile
