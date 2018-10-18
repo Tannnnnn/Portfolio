9113346fcf998aea051ec3cc5d827b882ebbf83c
Tannnnnn <tan_sweetmulet@hotmail.com>
2018-10-18 14:02:58 +0700

    initial commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
-
 export class Profile extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-Header">
-            Tan 🖤 Oily<br /><small>\(^o^)/</small>
-        </header>
+      <div>
+        <div className="App">
+          <header className="App-Header">
+              <div class="text animate-bottom">
+                <div class="r">Hello,I'm...Tan! <br/>
+                  <span className="small-text">
+                    I'm a Software Engineering. <br/>Get to know me!
+                  </span>
+                </div>
+                <div class="g">Hello,I'm...Tan! <br/>
+                  <span className="small-text">
+                    I'm a Software Engineering. <br/>Get to know me!            
+                  </span>
+                </div>
+                <div class="b">Hello,I'm...Tan! <br/>
+                  <span className="small-text">
+                    I'm a Software Engineering. <br/>Get to know me!             
+                  </span>
+                </div>
+              </div>
+          </header>
+        </div>
+        <p class="scroll-down animate-bottom">
+          <a href="#complements" class="animate"></a>
+        </p>
       </div>
     )
   }
+  
 }
 
 export default Profile
