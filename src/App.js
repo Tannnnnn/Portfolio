dfd47e6a3911548c2977cddbee96f4540358f5f7
kitpakorn <kitpakorn@thinknet.co.th>
2019-08-31 14:33:55 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React, { Component } from 'react';
 import './styles/Portfolio.css';
 import './styles/Parallax.scss';
 import Anonymous from './components/Anonymous.js'
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect
-} from "react-router-dom";
 
 class App extends Component {
   render() {    
     return (
       <div className="Parallax-bg">
-        <Router>
-          <div>
-              <Route exact path="/" component={Anonymous} />
-              <Redirect to="/" />
-          </div>
-        </Router>
+        <Anonymous />
       </div>
-      
     );
   }
 }
