053388e2884abd7bf1416595566bbd023c3154cd
kitpakorn <kitpakorn@thinknet.co.th>
2019-08-31 14:40:07 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './styles/Portfolio.css';
 import './styles/Parallax.scss';
 import Anonymous from './components/Anonymous.js'
 
+
 class App extends Component {
   render() {    
     return (
       <div className="Parallax-bg">
         <Anonymous />
       </div>
+      
     );
   }
 }
